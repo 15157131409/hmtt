acb17731ef3282713f2faaf92cd542a447bddd60
三号市民G <915770569@qq.com>
2022-06-26 11:44:21 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Layout from '@/views/layout'
+const Home = () => import('@/views/Home')
+const Video = () => import('@/views/Video')
+const Qa = () => import('@/views/Qa')
+const My = () => import('@/views/My')
+const Login = () => import('@/views/Login')
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      { path: 'home', component: Home },
+      { path: 'video', component: Video },
+      { path: 'qa', component: Qa },
+      { path: 'my', component: My }
 
+    ]
+  },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
